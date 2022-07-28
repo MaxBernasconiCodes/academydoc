79f712f606fb8d011e284d7647065021001b1c4b
“Max <max_bernasconi@hotmail.com>
2022-07-28 15:17:21 -0300

    correccion de modules

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -4,7 +4,6 @@ const Module = (props) => {
     let {name,title, imgPath} = props;
   return (
     <>
-    
       <label className="card">
         <lablel>{title}</lablel>
         <img src={'/logos/'+imgPath} alt={name} />
